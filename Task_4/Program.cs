7dbd6af2199d5df94668337b9af60c9fa7b4f3a5
Daniil <daniilkharitonov1990@gmail.com>
2022-11-05 00:47:49 +0500

    Добавлена задача 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+//Введенное число будет границей исчислений.
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 1; i <= a; i ++)
+{
+    if  (i % 2 ==0)
+    Console.WriteLine(Convert.ToInt32(i));
+}
