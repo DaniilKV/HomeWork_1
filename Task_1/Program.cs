00d2c36fe7abc5996c2ad5fd433555dc1e6cac4d
Daniil <daniilkharitonov1990@gmail.com>
2022-11-04 21:43:44 +0500

    Представлено решение задачи 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Task 1
+Console.WriteLine("Введите два числа");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write("max, ");
+    Console.Write(numberA);
+}
+else
+{
+    if (numberA == numberB)
+    {
+        Console.WriteLine("Числа равны");
+    }
+    else
+    {
+        Console.Write("max, ");
+        Console.Write(numberB);
+    }
+}
